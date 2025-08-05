863ddad0d2d66aef524cb2279c606586b7658d7c
LeFab70 <kouonang2002@gmail.com>
2025-08-05 11:07:24 -0300

    Initial Commit

---
+++ SwiftDataExpensesSave/SwiftDataExpensesSaveApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataExpensesSaveApp.swift
+//  SwiftDataExpensesSave
+//
+//  Created by Fabrice Kouonang on 2025-08-05.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataExpensesSaveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
