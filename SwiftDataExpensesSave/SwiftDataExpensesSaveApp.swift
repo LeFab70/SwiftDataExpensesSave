a9398b15cde0ba4d2f1dfff3a8ed7d7b32704f52
LeFab70 <kouonang2002@gmail.com>
2025-08-05 11:16:22 -0300

    models created

--- SwiftDataExpensesSave/SwiftDataExpensesSaveApp.swift
+++ SwiftDataExpensesSave/SwiftDataExpensesSaveApp.swift
@@ -6,12 +6,13 @@
 //
 
 import SwiftUI
-
+import SwiftData
 @main
 struct SwiftDataExpensesSaveApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+        .modelContainer(for:Expense.self)
     }
 }
--- SwiftDataExpensesSave/models/Expense.swift
+++ SwiftDataExpensesSave/models/Expense.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+import SwiftData
+
+@Model
+class Expense{
+    var name: String
+    var date : Date
+    var amount : Double
+    
+    init(name: String, date: Date=Date.now, amount: Double) {
+        self.name = name
+        self.date = date
+        self.amount = amount
+    }
+}
